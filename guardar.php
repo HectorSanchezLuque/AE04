5b2ccb24bd8ee12d9a96285a3535193420283c89
HectorSanchezLuque <92037326+HectorSanchezLuque@users.noreply.github.com>
2023-01-31 19:10:55 +0100

    Add files via upload

--- guardar.php
+++ guardar.php
@@ -1,25 +1,24 @@
 <?php
-if (isset($_POST["name"])) {
     $servidor = "localhost";
     $usuario = "root";
     $password = "";
     $dbname = "ae04";
     $conexion = mysqli_connect($servidor, $usuario, $password, $dbname);
 
-    $name = $_POST["name"];
+    $name = $_POST["nom"];
     $descripcio = $_POST["descripcio"];
     $tipus = $_POST["tipus"];
     $color = $_POST["color"];
     $cost = $_POST["cost"];
     $power = $_POST["power"];
     $resistencia = $_POST["resistencia"];
-    $image = $_POST["photo"];
+    $image = $_POST["image"];
 
     if (!$conexion) {
         echo "Error en la conexion a MySQL: " . mysqli_connect_error();
         exit();
     }
-    $sql = "INSERT INTO cartes (nom, descripcio, tipus, color, cost, poder, resistencia, foto) VALUES ('$name', '$descripcio', '$tipus', '$color', '$cost,' '$power', '$resistencia', '$image');";
+    $sql = "INSERT INTO cartes (nom, descripcio, tipus, color, cost, poder, resistencia, foto) VALUES ('".$name."','".$descripcio."','".$tipus."','".$color."','".$cost."','".$power."','".$resistencia."','".$image."')";
     if (mysqli_query($conexion, $sql)) {
         echo "Registro insertado correctamente.";
         echo "<br><a href='index.html'>Volver</a>";
@@ -27,4 +26,3 @@ if (isset($_POST["name"])) {
         echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
         echo "<br><a href='index.html'>Volver</a>";
     }
-}
